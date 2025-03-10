7c632bf8105a861b082d83d5efdc53df2ceec10e
naxo25 <y.labra.25@gmail.com>
2025-03-10 18:49:35 -0300

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,21 +13,8 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      '@typescript-eslint/ban-ts-comment': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: false,
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^(_|ignore)',
-        },
-      ],
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off'
     },
   },
   {
